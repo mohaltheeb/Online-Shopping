2c1aefb4e79668c2ba01898c1263dc0e75b4c25c
mohaltheeb <midoaltheeb@gmail.com>
2023-12-14 06:57:29 +0300

    Createed a Footer Page

---
+++ footer.php
@@ -0,0 +1,41 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">home</a>
+         <a href="about.php">about</a>
+         <a href="shop.php">shop</a>
+         <a href="contact.php">contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">login</a>
+         <a href="register.php">register</a>
+         <a href="cart.php">cart</a>
+         <a href="orders.php">orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> (+967) 777 620 806 </p>
+         <p> <i class="fas fa-phone"></i> (+967) 775355805 </p>
+         <p> <i class="fas fa-envelope"></i> info@ema.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> SANAA - YEMEN  </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span> E M A - Development </span> </p>
+
+</section>
